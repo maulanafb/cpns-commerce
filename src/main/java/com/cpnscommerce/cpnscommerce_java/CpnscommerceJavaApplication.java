17a81f8c2e31e6132791081c0f2e1a7e3ee93875
maulanafb <gagas112212@gmail.com>
2024-11-28 17:01:58 +0700

    init commit

---
+++ src/main/java/com/cpnscommerce/cpnscommerce_java/CpnscommerceJavaApplication.java
@@ -0,0 +1,14 @@
+package com.cpnscommerce.cpnscommerce_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpnscommerceJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CpnscommerceJavaApplication.class, args);
+		System.out.println("Hello, World!");
+	}
+
+}
---
+++ src/test/java/com/cpnscommerce/cpnscommerce_java/CpnscommerceJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cpnscommerce.cpnscommerce_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CpnscommerceJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
